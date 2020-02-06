0313ee1862e6a287841085cecb64dc462968fa23
Renato Pacheco <renato.pacheco.r2@gmail.com>
2020-02-06 18:37:28 -0300

    add some components in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './App.css';
+
+function Container(props) {
+  return (<div className="container">{props.children}</div>);
+}
+
+function Item() {
+  return (
+    <>
+      <img width="80" height="80" src="https://images-na.ssl-images-amazon.com/images/I/41nE19Ab-KL.png" />
+      <p>Say hello to nyan cat</p>
+    </>
+  );
+}
 
 function App() {
   return (
     <div id="app">
       <main>
-        <h1>Ola brother</h1>
+        <Container>
+          <Item />
+        </Container>
       </main>
     </div>
   );
