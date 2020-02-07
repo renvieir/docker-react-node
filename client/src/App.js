dda0a1e48de22c7f46cf483c7b2429deea652287
Renato Pacheco <renato.pacheco.r2@gmail.com>
2020-02-07 09:59:53 -0300

    add responsive layout and change buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
+import './Main.css';
 import Item from './Item';
 
 function App() {
   return (
     <div id="app">
       <main>
-        <div className="container">
+        <ul>
           <Item imgSrc="https://qa-m1-dr.abi-sandbox.net/media/catalog/product/-/R/-R002151.png" />
           <Item imgSrc="https://qa-m1-dr.abi-sandbox.net/media/catalog/product/-/R/-R002151.png" />
           <Item imgSrc="https://qa-m1-dr.abi-sandbox.net/media/catalog/product/-/R/-R002151.png" />
           <Item imgSrc="https://qa-m1-dr.abi-sandbox.net/media/catalog/product/-/R/-R002151.png" />
-        </div>
+        </ul>
       </main>
     </div>
   );
